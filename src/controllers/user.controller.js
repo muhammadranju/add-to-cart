0ecb2e5a739904b038c3a9cebaf89b2d7981d1d3
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-01-03 10:30:36 +0600

    user controller fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,38 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const userLoginGetController = async (req, res, next) => {};
-const userLoginPostController = async (req, res, next) => {};
+const userLoginPostController = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(200).json({ error: "All field are required" });
+    }
+
+    const findUser = await User.findOne({ email });
+    if (!findUser) {
+      return res.status(200).json({ error: "Invalid email or password" });
+    }
+
+    const payload = {
+      id: findUser._id,
+      username: findUser.username,
+      fullName: findUser?.full_name,
+      email: findUser.email,
+    };
+
+    const token = await jwt.sign(
+      payload,
+      "mnfjdn56kndf%*(#&%&$df5d7dsfjksdhnfsdfjhsdf4hs41f"
+    );
+
+    const matchPassword = await bcrypt.compare(password, findUser.password);
+    if (!matchPassword) {
+      return res.status(200).json({ error: "Invalid email or password" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const userRegisterGetController = async (req, res, next) => {};
 const userRegisterPostController = async (req, res, next) => {
