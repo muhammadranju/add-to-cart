06e1b2dc4d684643410402ffeb991d49c9fd7b63
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-01-04 00:22:48 +0600

    uer controller many changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,16 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const ApiError = require("../utils/ApiError");
+const ApiResponse = require("../utils/ApiResponse");
+const asyncHandler = require("../utils/asyncHandler");
 
 const userLoginGetController = async (req, res, next) => {};
-const userLoginPostController = async (req, res, next) => {
+const userLoginPostController = asyncHandler(async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
+      throw new ApiError(400, "All field are required");
       return res.status(200).json({ error: "All field are required" });
     }
 
@@ -22,7 +26,7 @@ const userLoginPostController = async (req, res, next) => {
       email: findUser.email,
     };
 
-    const token = await jwt.sign(
+    const accessToken = await jwt.sign(
       payload,
       "mnfjdn56kndf%*(#&%&$df5d7dsfjksdhnfsdfjhsdf4hs41f"
     );
@@ -31,37 +35,52 @@ const userLoginPostController = async (req, res, next) => {
     if (!matchPassword) {
       return res.status(200).json({ error: "Invalid email or password" });
     }
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .json(new ApiResponse(200, { accessToken }, "User login successfully"));
   } catch (error) {
     next(error);
   }
-};
+});
 
 const userRegisterGetController = async (req, res, next) => {};
 const userRegisterPostController = async (req, res, next) => {
   try {
-    const { username, full_name, email, password } = req.body;
+    const { username, fullName, email, password } = req.body;
 
     if ((!username, !email, !password)) {
-      return res.status(400).json({ error: "All fields are required" });
+      throw new ApiError(400, "All fields are required");
     }
-    console.log(req.body);
+
     const findUser = await User.findOne({ email });
 
     if (findUser) {
-      return res.status(400).json({ error: "this email already exits" });
+      throw new ApiError(400, "this email already exits");
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       username,
-      full_name,
+      fullName,
       email,
       password: hashPassword,
     });
+    const data = {
+      username: user.username,
+      fullName: user.fullName,
+      email: user.email,
+    };
+    await user.save();
     return res
       .status(201)
-      .json({ data: user, message: "User create successfully" });
+      .json(new ApiResponse(201, data, "User create successfully"));
   } catch (error) {
     next(error);
   }
