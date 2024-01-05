94acfe8bbd2e01f92b527e78168219f8e88e0c00
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-01-05 18:49:53 +0600

    update product.controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,8 +1,27 @@
 const Product = require("../models/product.model");
+const ApiError = require("../utils/ApiError");
+const asyncHandler = require("../utils/asyncHandler");
 
-const allProductGetController = async (req, res, next) => {
+const allProductGetController = asyncHandler(async (req, res, next) => {
   const products = await Product.find();
   return res.render("pages/index", { products });
-};
+});
 
-module.exports = { allProductGetController };
+const getProductById = asyncHandler(async (req, res) => {
+  try {
+    const { productID } = req.params;
+    if (!productID) {
+      throw new ApiError(404, "this product is not available");
+    }
+
+    const product = await Product.findById({ _id: productID });
+    if (!product) {
+      throw new ApiError(404, "this product is not available");
+    }
+    return res.status(200).render("pages/product", { product });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = { allProductGetController, getProductById };
