eb3c407974c90c49a0d7bb1230c12fdc73ae9add
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-01-17 01:58:09 +0600

    add ApiVersion in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-
+const { ApiVersion } = require("../constants");
 const cartRoute = require("./cart.routes");
 const productRoute = require("./product.routes");
 const userRoute = require("./user.routes");
 
-router.use("/api/v1/add-to-cart", cartRoute);
-router.use("/api/v1/products", productRoute);
-router.use("/api/v1/user", userRoute);
+router.use(`${ApiVersion}/add-to-cart`, cartRoute);
+router.use(`${ApiVersion}/products`, productRoute);
+router.use(`${ApiVersion}/`, userRoute);
 
 module.exports = router;
